5f02a7945015f08d4875e7e518b2bbee745e436b
yeGenieee <cyj95428@gmail.com>
2021-05-28 16:43:17 +0900

    feat: Member 엔티티 & MemberJpaRepository 구현

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,32 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member (String username) {
+        this.username = username;
+    }
+
+    public void changeUserName(String username) {
+        this.username = username;
+    }
+}
---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,36 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @DisplayName("멤버 테스트")
+    void testMember() throws Exception {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        // when
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
