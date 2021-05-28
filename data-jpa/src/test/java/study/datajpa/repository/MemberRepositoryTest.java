4486bdbf7dda99a3a790daf32fcbc2f56cca94b6
yeGenieee <cyj95428@gmail.com>
2021-05-28 21:17:22 +0900

    feat: Spring Data JPA 에서의 벌크성 수정 쿼리 구현

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -261,4 +261,22 @@ class MemberRepositoryTest {
         assertThat(page.hasNext()).isTrue();
     }
 
+    @Test
+    @DisplayName("bulkUpdate 테스트")
+    void bulkUpdate() throws Exception {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        // when
+        int resultCount = memberRepository.bulkAgePlus(20);
+
+        // then
+        assertThat(resultCount).isEqualTo(3);
+
+    }
+
 }
